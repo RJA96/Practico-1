20edd3f1ce644192097af6657a46204cdc0502e9
Rodrigo <rodrigoaguilar96@gmail.com>
2019-09-01 20:36:36 -0300

    corrections point 5

--- js/main.js
+++ js/main.js
@@ -1,30 +1,35 @@
 document.querySelector(".punto2").addEventListener("click", () => {
+    event.preventDefault();
     let ht = document.getElementById("use-async");
     asyncro(ht, "punto_2.html").then(function () {
         canvaspunto2();
     })
 })
 document.querySelector(".punto3").addEventListener("click", () => {
+    event.preventDefault();
     let ht = document.getElementById("use-async");
     asyncro(ht, "punto_3.html").then(function () {
         canvaspunto3();
     })
 })
 document.querySelector(".punto4").addEventListener("click", () => {
+    event.preventDefault();
     let ht = document.getElementById("use-async");
     asyncro(ht, "punto_4.html").then(function () {
         canvaspunto4();
     })
 })
 document.querySelector(".punto5").addEventListener("click", () => {
+    event.preventDefault();
     let ht = document.getElementById("use-async");
     asyncro(ht, "punto_5.html").then(function () {
         canvaspunto5();
     })
 })
 document.querySelector(".punto6").addEventListener("click", () => {
+    event.preventDefault();
     let ht = document.getElementById("use-async");
-    asyncro(ht, "punto_6.html").then(function () {        
+    asyncro(ht, "punto_6.html").then(function () {
         canvaspunto6();
     })
 })
@@ -34,6 +39,7 @@ function canvaspunto2() {
     let canvas = document.getElementById('canv');
     let ctx = canvas.getContext("2d");
     document.querySelector("#punto2submit").addEventListener("click", function () {
+        event.preventDefault();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         canvas.height = document.querySelector("#height").value;
         canvas.width = document.querySelector("#width").value;
@@ -46,6 +52,7 @@ function canvaspunto3() {
     let canvas = document.getElementById('canv');
     let ctx = canvas.getContext("2d");
     document.querySelector("#punto3submit").addEventListener("click", function () {
+        event.preventDefault();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         let image = ctx.createImageData(document.querySelector("#width").value, document.querySelector("#height").value);
         canvas.width = image.width;
@@ -74,15 +81,16 @@ function canvaspunto4() {
     let canvas = document.getElementById('canv');
     let ctx = canvas.getContext("2d");
     document.querySelector("#punto4submit").addEventListener("click", function () {
+        event.preventDefault();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         let width = document.querySelector("#width").value;
         let height = document.querySelector("#height").value;
-        let image = ctx.createImageData(width,height);
+        let image = ctx.createImageData(width, height);
         canvas.width = image.width;
         canvas.height = image.height;
         for (let x = 0; x < image.width; x++) {
             for (let y = 0; y < image.height; y++) {
-                let r,g,b;
+                let r, g, b;
                 r = y / (height) * 255;
                 g = y / (height) * 255;
                 b = y / height * 255;
@@ -90,32 +98,35 @@ function canvaspunto4() {
             }
         }
         ctx.putImageData(image, 0, 0);
-    });  
+    });
 }
 
 function canvaspunto5() {
     let canvas = document.getElementById('canv');
     let ctx = canvas.getContext("2d");
     document.querySelector("#punto5submit").addEventListener("click", function () {
+        event.preventDefault();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         let width = document.querySelector("#width").value;
         let height = document.querySelector("#height").value;
-        let image = ctx.createImageData(width,height);
+        let image = ctx.createImageData(width, height);
         canvas.width = image.width;
         canvas.height = image.height;
+        let r, g, b;
         for (let x = 0; x < image.width; x++) {
-            for (let y = 0; y < image.height; y++) {
-                let r, g, b;
-                if (y < image.height / 2) {
-                    r = y / (image.height / 2) * 255;
-                    g = y / (image.height / 2) * 255;
-                    b = y / image.height * 0;
+            
+                if (x < image.width / 2) {
+                    r =x / (image.width / 2) * 255;
+                    g = x / (image.width / 2) * 255;
+                    b = x / image.width * 0;
                 } else {
                     r = 255;
-                    g = (1 - ((y - image.height / 2) / (image.height / 2))) * 255;
+                    g = (1 - ((x - image.width / 2) / (image.width / 2))) * 255;
                     b = 0;
                 }
-                setpixel(image, y, x, r, g, b, 255);
+            for (let y = 0; y < image.height; y++) {
+                
+                setpixel(image, x, y, r, g, b, 255);
             }
         }
         ctx.putImageData(image, 0, 0)
@@ -136,32 +147,37 @@ function canvaspunto6() {
     document.querySelector("#punto5submit").addEventListener("click", function () {
         event.preventDefault();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        if (document.querySelector("#subir").value==='') {
-            if (document.querySelector("#basic-url").value !='') {
-                let img = new Image();
-                img.crossOrigin = "Anonymous";
-                img.src = document.querySelector("#basic-url").value;
-                img.onload = function () {
-                    canvas.height = img.height;
-                    canvas.width = img.width;
-                    ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
-                    document.querySelector(".form-canvas").style.display = "none";
-                    document.querySelector(".escala_grises").style.display = "block";
-                    document.querySelector("#filtrsubmit").addEventListener("click", function () {  
-                        event.preventDefault();
-                        let imagedata = ctx.getImageData(0,0,img.width,img.height); 
-                        if (document.getElementsByName("opciones")[1].checked){
-                            escala_grises(1, imagedata);
-                        }
-                        else{
-                            escala_grises(0, imagedata);
-                        }
-                        ctx.putImageData(imagedata, 0, 0);
+        if (document.querySelector("#basic-url").value != '') {
+            let img = new Image();
+            //img.crossOrigin = "Anonymous";
+            img.src = document.querySelector("#basic-url").value;
+            img.onload = function () {
+                canvas.height = img.height;
+                canvas.width = img.width;
+                ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
+                document.querySelector(".form-canvas").style.display = "none";
+                document.querySelector(".escala_grises").style.display = "block";
+                document.querySelector(".Cargarotra").addEventListener("click", function () {
+                    event.preventDefault();
+                    let ht = document.getElementById("use-async");
+                    asyncro(ht, "punto_6.html").then(function () {
+                        canvaspunto6();
                     })
+                })
+                
+                document.querySelector("#filtrsubmit").addEventListener("click", function () {
+                    event.preventDefault();
+                    let imagedata = ctx.getImageData(0, 0, canvas.width, canvas.height);
+                    if (document.getElementsByName("opciones")[1].checked) {
+                        escala_grises(1, imagedata);
+                    } else {
+                        escala_grises(0, imagedata);
+                    }
+                    ctx.putImageData(imagedata, 0, 0);
+                })
+
             }
-        }  
-        }
-        else{
+        } else {
             let img = new Image();
             img.crossOrigin = "Anonymous";
             img.src = document.querySelector("#subir").value;
@@ -171,45 +187,42 @@ function canvaspunto6() {
                 ctx.drawImage(img, 0, 0, img.width, img.height, 0, 0, canvas.width, canvas.height);
             }
         }
-        
-
-
     });
 }
-function getRed(imageData,x,y){
-    index = (x+y*imageData.width)*4;
-    return imageData.data[index+0];
+
+function getRed(imageData, x, y) {
+    index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 0];
 }
-function getGreen(imageData,x,y){
-    index = (x+y*imageData.width)*4;
-    return imageData.data[index+1];
+
+function getGreen(imageData, x, y) {
+    index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 1];
 }
-function getBlue(imageData,x,y){
-    index = (x+y*imageData.width)*4;
-    return imageData.data[index+2];
+
+function getBlue(imageData, x, y) {
+    index = (x + y * imageData.width) * 4;
+    return imageData.data[index + 2];
 }
-function escala_grises(opcion,img){ 
-     
+
+function escala_grises(opcion, img) {
+
     for (let x = 0; x < img.width; x++) {
         for (let y = 0; y < img.height; y++) {
-            
-            let red = getRed(img,x,y);
-            let green = getGreen (img,x,y);
-            let blue = getBlue (img,x,y);
-            let gris
-            if (opcion==0){
-                gris = (red+green+blue)/3;
-            }
-            else{
-                gris = (red*0.33+green*0.5+0.15*blue);
-            }
-            setpixel(img,x,y,gris,gris,gris,255);
-        }
-        
-    }
 
-   // ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
+            let red = getRed(img, x, y);
+            let green = getGreen(img, x, y);
+            let blue = getBlue(img, x, y);
+            let gris
+            if (opcion == 0) {
+                gris = (red + green + blue) / 3;
+            } else {
+                gris = (red * 0.33 + green * 0.5 + 0.15 * blue);
+            }
+            setpixel(img, x, y, gris, gris, gris, 255);
+        }
+
+    }
 }
 async function asyncro(h, url) {
     let response = await fetch(url);
